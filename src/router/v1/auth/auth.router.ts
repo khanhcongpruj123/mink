a587014c166a21f24710187ad62e0e57c74d261c
idev <khanhcongpruj123@gmail.com>
2022-08-09 23:53:53 +0700

    Fix response data

--- src/router/v1/auth/auth.router.ts
+++ src/router/v1/auth/auth.router.ts
@@ -9,7 +9,12 @@ router.post("/auth/sign-up", async (request: Request, response: Response) => {
         username: request.body.username,
         password: request.body.password
     };
-    const user = await userService.createUser(createUserCommand);
+    try {
+        const user = await userService.createUser(createUserCommand);
+    } catch (error) {
+        response.sendStatus(400);
+    }
+    response.status(204);
 });
 
 export default router;
