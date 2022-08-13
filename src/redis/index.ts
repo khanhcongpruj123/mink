05ebbaadf18f9e8364628a341976cc80f7364145
idev <khanhcongpruj123@gmail.com>
2022-08-13 17:57:41 +0700

    Refactor code redis client

--- src/redis/index.ts
+++ src/redis/index.ts
@@ -2,10 +2,12 @@ import { createClient } from '@redis/client';
 import Logger from '../lib/logger';
 
 const REDIS_PASSWORD = process.env.REDIS_PASSWORD || "mink123";
+const REDIS_HOST = process.env.REDIS_HOST || "localhost";
+const REDIS_PORT = process.env.REDIS_PORT || "6379";
 
 const client = createClient({
-    url: "redis://localhost:6379",
-    password: REDIS_PASSWORD
+    url: `redis://${REDIS_HOST}:${REDIS_PORT}`,
+    password: REDIS_PASSWORD,
 });
 
 client.on("error", (err) => {
