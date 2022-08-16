6f583ad19e3c6fa09bd44f45bdf2b9a945fdce6e
idev <khanhcongpruj123@gmail.com>
2022-08-16 15:01:06 +0700

    Update validate message response

--- src/middlewares/validator.middleware.ts
+++ src/middlewares/validator.middleware.ts
@@ -1,6 +1,6 @@
 import { NextFunction, Request, RequestHandler, Response } from "express";
 import { plainToInstance } from "class-transformer";
-import { validate } from "class-validator";
+import { validate, ValidationError } from "class-validator";
 import { BadRequest } from "@core/error";
 
 const validatorMiddleware = (type: any): RequestHandler => {
@@ -8,12 +8,19 @@ const validatorMiddleware = (type: any): RequestHandler => {
     const body = plainToInstance(type, req.body);
     validate(body).then((errors) => {
       if (errors.length > 0) {
-        next(BadRequest(errors[0].constraints.toString()));
+        next(BadRequest(errors.map(getAllNestedErrors).join(", ")));
       } else {
         next();
       }
     });
   };
+};
+
+const getAllNestedErrors = (error: ValidationError) => {
+  if (error.constraints) {
+    return Object.values(error.constraints);
+  }
+  return error.children.map(getAllNestedErrors).join(", ");
 };
 
 export default validatorMiddleware;
